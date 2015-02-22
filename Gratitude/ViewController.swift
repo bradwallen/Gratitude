5e50f6c0e42cd87dd1d3aee612ee010b6cd2382e
Brad W. Allen <ballen12@gmail.com>
2015-02-22 14:42:01 -0600

    Initial Commit

---
+++ Gratitude/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gratitude
+//
+//  Created by Brad Allen on 2/22/15.
+//  Copyright (c) 2015 Studio M-Five. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
